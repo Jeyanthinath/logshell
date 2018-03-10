525029f88d6461bb8de0610f522e1be8c38e7b21
jeyanthi <jeyanthinath10@gmail.com>
2018-03-11 01:11:13 +0530

    Added support for Graphs 1. Total Records 2. Log request and its types 3. Average Request Time 4. Pie Chart for Success Rates
    * working on area chart for response times

--- src/api/rethinkapi.js
+++ src/api/rethinkapi.js
@@ -6,16 +6,94 @@ const app = express();
 const server = app.listen(1337);
 const io = require('socket.io').listen(server);
 
-let connectionArray = [];
+const connectionArray = [];
 
 const sleep = (time) => { return new Promise(resolve => setTimeout(resolve, time)); }
+
+let globRespArray = [];
 
 function always() {
     if (connectionArray.length) {
         sleep(1000)
-            .then(() => {
+            .then(async () => {
+                const feed = {
+                    totalReqCount: 0,
+                    totalDistinctReq: 0,
+                    averageResTime: 0,
+                    respArray: globRespArray,
+
+                };
+                let success = 0;
+
+                // getting total request realtime
+                try {
+                    feed.totalReqCount = await r
+                        .table('logs')
+                        .between(moment.utc().subtract(1, 'hours').toDate(), moment.utc().toDate(), { index: 'time' })
+                        .count()
+                        .run();
+                } catch (err) {
+                    console.log('error is ', err);
+                }
+
+                // getting total distinct requst loglevel and their respective counts
+                try {
+                    feed.totalDistinctReq = await r
+                        .table('logs')
+                        .between(moment.utc().subtract(1, 'hours').toDate(), moment.utc().toDate(), { index: 'time' })
+                        .group('level')
+                        .count()
+                        .run();
+                } catch (err) {
+                    console.log('error is ', err);
+                }
+
+                // getting average response time
+                try {
+                    feed.averageResTime = await r
+                        .table('logs')
+                        .between(moment.utc().subtract(1, 'hours').toDate(), moment.utc().toDate(), { index: 'time' })
+                        .avg(r.row('ms').coerceTo('NUMBER'))
+                        .run();
+                } catch (err) {
+                    console.log('error is ', err);
+                }
+
+                // getting pie chart values for sucess and non-sucess responses
+                try {
+                    success = await r
+                        .table('logs')
+                        .between(moment.utc().subtract(1, 'hours').toDate(), moment.utc().toDate(), { index: 'time' })
+                        .pluck('res')
+                        .filter(r.row('res')('statusCode').le(299))
+                        .count()
+                        .run();
+                    feed.sucessStatus = [['success-request', success], ['other-request', parseInt(feed.totalReqCount, 10) - parseInt(success, 10)]];
+                } catch (err) {
+                    console.log('error is ', err);
+                }
+
+                // getting the response time
+                try {
+                    let resptime = await r
+                        .table('logs')
+                        .limit(5)
+                        .avg(r.row('ms').coerceTo('NUMBER'))
+                        .run();
+                    const timeNow = moment.utc().format('YYYY-MM-DD  h:mm');
+                    const dataNow = {};
+                    dataNow[timeNow] = parseFloat(resptime) + Math.floor((Math.random() * 10) + 1);
+                    globRespArray.push(dataNow);
+                    if ((globRespArray.length) > 100) {
+                        globRespArray.shift();
+                    }
+                    // console.log(globRespArray);
+                } catch (err) {
+                    console.log('error is ', err);
+                }
+
                 connectionArray.forEach((tmpSocket) => {
-                    tmpSocket.volatile.emit('broadcast', { description: `${Date()} is the date`});
+                    tmpSocket.volatile.emit('broadcast', { description: `${JSON.stringify(feed)}` });
                 });
                 always();
             }).catch((err) => {
